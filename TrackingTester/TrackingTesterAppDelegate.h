66c44b8225307003ededb58fc32683e799f24048
Bryan Everly <bryan@bceassociates.com>
2011-05-19 08:03:56 -0400

    Initial Commit

---
+++ TrackingTester/TrackingTesterAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TrackingTesterAppDelegate.h
+//  TrackingTester
+//
+//  Created by Bryan Everly on 5/19/11.
+//  Copyright 2011 BCE Associates, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrackingTesterAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ TrackingTester/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TrackingTester
+//
+//  Created by Bryan Everly on 5/19/11.
+//  Copyright 2011 BCE Associates, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
